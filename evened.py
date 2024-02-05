a184148eed4095fc0d9a8aae3bdb34d6e7c86af7
adarshgurjarc <158771139+adarshkumarc@users.noreply.github.com>
2024-02-05 22:53:50 +0530

    Update evened.py
    add new program

--- evened.py
+++ evened.py
@@ -3,3 +3,23 @@ if(number%2)==0:
     print("even number")
 else:
     print("odd number")
+#tempreture humidity
+temperature = "Cold"
+humidity = "Humid"
+
+if temperature == "Cold":
+    if humidity == "Dry":
+        print("It's cold and dry.")
+    elif humidity == "Humid":
+        print("It's cold and humid.")
+    else:
+        print("Invalid humidity value.")
+elif temperature == "Warm":
+    if humidity == "Dry":
+        print("It's warm and dry.")
+    elif humidity == "Humid":
+        print("It's warm and humid.")
+    else:
+        print("Invalid humidity value.")
+else:
+    print("Invalid temperature value.")
